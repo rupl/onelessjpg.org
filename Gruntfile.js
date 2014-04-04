23de5b098b73ec37b9536a06e219f177e92c4051
Chris Ruppel <chris.ruppel@gmail.com>
2014-04-04 09:22:53 +0200

    :boom: initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,119 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    clean: {
+      css: 'css',
+      js: 'js',
+    },
+
+    jshint: {
+      options: {},
+      dist: {
+        files: {
+          src: ['Gruntfile.js', 'src/**/*.js']
+        }
+      },
+    },
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+      css: {
+        src: ['src/css/main.css', 'src/css/syntax.css'],
+        dest: 'css/main.css',
+      },
+    },
+
+    autoprefixer: {
+      options: {},
+      default: {
+        src: 'css/main.css'
+      },
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          'js/final.min.js': ['js/poop.js']
+        }
+      }
+    },
+
+    imagemin: {
+      dist: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          cwd: 'img',
+          src: ['**/*.png', '**/*.jpg'],
+          dest: 'src/img'
+        }]
+      }
+    },
+
+    svgmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'img',
+          src: '**/*.svg',
+          dest: 'src/img'
+        }]
+      }
+    },
+
+    concurrent: {
+        default: {
+            tasks: ['jekyll:serve', 'watch'],
+            options: {
+                logConcurrentOutput: true
+            }
+        }
+    },
+
+    watch: {
+      css: {
+        files: ['src/css/*.css'],
+        tasks: ['autoprefixer']
+      },
+      js: {
+        files: ['src/js/*.js'],
+        tasks: ['jshint', 'uglify']
+      },
+    },
+
+    jekyll: {
+      serve: {
+        options: {
+          serve: true,
+          server_port: 4000,
+          watch: true,
+        },
+      },
+    }
+  });
+
+  // Load Plugins.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-svgmin');
+  grunt.loadNpmTasks('grunt-jekyll');
+
+  // Custom tasks.
+  grunt.registerTask('watchCss', ['clean', 'concat', 'autoprefixer']);
+
+  // Default task.
+  grunt.registerTask('default', ['concurrent:default']);
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# One Less JPG
+
+A harmless little site to show you how small your least favorite JS library is.
